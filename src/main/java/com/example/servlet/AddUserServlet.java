d2c3388146104b54fbe2a8281f4229860791b913
Iskender <mederov.iskendr@gmail.com>
2023-11-25 17:18:08 +0600

    commit

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,29 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        User newUser = new User(firstName, lastName);
+        Warehouse.getInstance().addUser(newUser);
+
+        request.setAttribute("user", newUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/add");
+        dispatcher.forward(request, response);
+    }
 }
